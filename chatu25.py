b5a51fba269c1ca367841edcbe2f94d11ca07dd4
Chaturya3125 <48007395+Chaturya3125@users.noreply.github.com>
2019-02-28 14:47:07 +0530

    Add files via upload

---
+++ chatu25.py
@@ -0,0 +1,39 @@
+
+import random
+user=0
+comp=0
+l=["r","p","s"]
+while True:
+	#take input from user
+	u=input("enter your choice,press n to discontinue")
+	#to exit
+	if u=='n':
+		print("Game over")
+		print("user score is:",user)
+		print("computer score is:",comp)
+		exit()
+	#input from computer
+	c=random.choice(l)
+	print ("computer chooses",c)
+
+	#compare the user and computer choice
+	if u == c:
+		print("tie")
+	elif u=="r" and c=="p":
+		print("comp wins")
+		comp=comp+1
+	elif u== 'r' and c =='s':
+		print("user wins")
+		user=user+1
+	elif u=="s" and c=="r":
+		print("comp wins")
+		comp=comp+1
+	elif u=="s" and c=='p':
+		print("user wins")
+		user=user+1
+	elif u=="p" and c=="s":
+		print("comp wins")
+		comp=comp+1
+	elif u=="p" and c=='r':
+		print("user wins")
+		user=user+1
